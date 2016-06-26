9b4a021198b5e9454980e922dbf6d0afbe10de3f
Ganesh <ganesh.kamalpur@gmail.com>
2016-06-26 21:02:46 +0530

    Updated with comments

--- LDAPAuthentication/src/main/java/com/security/ldap/controller/ViewController.java
+++ LDAPAuthentication/src/main/java/com/security/ldap/controller/ViewController.java
@@ -5,26 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ * A Basic controller for secured and public url access
+ *
+ */
 @Controller
 public class ViewController {
-
-	@RequestMapping(value = { "/", "/index**" }, method = RequestMethod.GET)
-	public ModelAndView publicPage() {
-
+	
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public ModelAndView publicURL() {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("index");
 		return model;
-
 	}
 
-	@RequestMapping(value = "/securedaccess**", method = RequestMethod.GET)
-	public ModelAndView securePage() {
-
+	@RequestMapping(value = "/securedaccess", method = RequestMethod.GET)
+	public ModelAndView secureURL() {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("secured");
-
 		return model;
-
 	}
 
 }
